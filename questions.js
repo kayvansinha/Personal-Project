9825ea4c5c0b1b4103298b5e7b036202e78e32e2
kayvansinha <97866504+kayvansinha@users.noreply.github.com>
2022-01-17 20:03:38 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How does crypto increase in value?",
+    answer: "Demand Increases",
+    options: [
+      "Owners Add More Coins",
+      "Demand Increases",
+      "Supply Increases",
+      "Demand Decreases"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is a great risk?",
+    answer: "The Volatility",
+    options: [
+      "Decentralizaton",
+      "No Middlemen",
+      "The Volatility",
+      "The Unbanked"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is a benefit?",
+    answer: "Encryption",
+    options: [
+      "More Money",
+      "The Volatility",
+      "Cybercrime",
+      "Encryption"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How to predict rises?",
+    answer: "Pricing History",
+    options: [
+      "Pricing History",
+      "Guess",
+      "Ask Random Person",
+      "Don't & Be Hopeful"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where is your crypto stored?",
+    answer: "Digital Wallet",
+    options: [
+      "Real Life Wallet",
+      "Digital Wallet",
+      "Bank",
+      "Government"
+    ]
+  },
+];
